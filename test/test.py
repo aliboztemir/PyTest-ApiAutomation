f5553e27cc1f03a79f62d325b156197bb48a4583
Ali Boztemir <aliboztemir@gmail.com>
2021-11-09 02:22:51 +0300

    second test case

--- test/test.py
+++ test/test.py
@@ -5,7 +5,7 @@ import json
 client = TestClient(app)
 
 
-def test_read_main():
+def test_match_id_deliver_planned():
     response_deliveries = client.get('/deliveries')
     input_dict_deliveries = json.loads(response_deliveries.text)
     output_dict_deliveries = [x['id'] for x in input_dict_deliveries if x['current_state'] == 'planned']
@@ -16,6 +16,15 @@ def test_read_main():
 
     for val in output_dict_deliveries:
         print(val in json_format_route['planned_route']['deliveries'])
+
+def test_check_carrying_capacity():
+    response_route = client.get('/route')
+    input_dict_route = json.loads(response_route.text)
+    output_dict_route = input_dict_route['planned_route']['deliveries']['algorithm_fields']['algorithm_fields']['weight']
+    print(output_dict_route)
+    #print(sum(info['weight'] for info in input_dict_route['planned_route']['deliveries']['algorithm_fields']['algorithm_fields']))
+
+
 
 
 
