ddaf92c4b011b13bea2bcaa59bdc9f07872baa0a
Ali Boztemir <aliboztemir@gmail.com>
2021-11-09 14:58:43 +0300

    test scenarios of the first question a and b

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 from starlette.testclient import TestClient
 from app.main import app
 import json
@@ -12,19 +14,41 @@ def test_match_id_deliver_planned():
 
     response_route = client.get('/route')
     json_format_route = json.loads(response_route.text)
+    output_dict_routes = [x['id'] for x in json_format_route['planned_route']['deliveries']]
 
+    print("Planned Routes")
+    for val in output_dict_routes:
+        print(val)
 
+    print("Delivery List")
     for val in output_dict_deliveries:
-        print(val in json_format_route['planned_route']['deliveries']['algorithm_fields'])
+        print(val)
+
+    exist_list = list(filter(lambda x: x in output_dict_routes, output_dict_deliveries))
+    print("exist_list")
+
+    for val in exist_list:
+        print(val)
+
+    def compare(s, t):
+        return sorted(s) == sorted(t)
+
+    assert compare(exist_list, output_dict_deliveries)
+
 
 def test_check_carrying_capacity():
     response_route = client.get('/route')
     input_dict_route = json.loads(response_route.text)
-    output_dict_route = input_dict_route['planned_route']['deliveries']['algorithm_fields']['algorithm_fields']['weight']
-    print(output_dict_route)
-    #print(sum(info['weight'] for info in input_dict_route['planned_route']['deliveries']['algorithm_fields']['algorithm_fields']))
 
+    carrying_capacity = input_dict_route['planned_route']['resource']['carrying_capacity']
+    print("carrying_capacity :", carrying_capacity)
 
+    print("weights")
+    weights = [x['algorithm_fields']['weight'] for x in input_dict_route['planned_route']['deliveries'] if
+               'weight' in x['algorithm_fields']]
+    for val in weights:
+        print(val)
+    print("sum")
+    print("sum", sum(weights))
 
-
-
+    assert carrying_capacity >= sum(weights)
