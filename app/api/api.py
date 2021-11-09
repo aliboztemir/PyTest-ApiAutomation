ca4ac37666e8f79c320904efd4d0016a5bac59f9
Ali Boztemir <aliboztemir@gmail.com>
2021-11-09 22:43:09 +0300

    all test scenarios of the first question

--- app/api/api.py
+++ app/api/api.py
@@ -2,14 +2,14 @@ import json
 
 
 def deliveries_for_planning():
-    with open('../data/deliveries_for_planning.json') as stream:
+    with open('data/deliveries_for_planning.json') as stream:
         deliveries = json.load(stream)
 
     return deliveries
 
 
 def planned_routes():
-    with open('../data/planned_route.json') as stream:
+    with open('data/planned_route.json') as stream:
         routes = json.load(stream)
 
     return routes
