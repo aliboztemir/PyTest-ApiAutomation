8172c0ec1abd396d9c484b1017242ce45375646f
Ali Boztemir <aliboztemir@gmail.com>
2021-11-09 22:42:05 +0300

    all test scenarios of the first question

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
+import datetime
+
 from starlette.testclient import TestClient
 from app.main import app
 import json
@@ -14,19 +16,7 @@ def test_match_id_deliver_planned():
     json_format_route = json.loads(response_route.text)
     output_dict_routes = [x['id'] for x in json_format_route['planned_route']['deliveries']]
 
-    print("Planned Routes")
-    for val in output_dict_routes:
-        print(val)
-
-    print("Delivery List")
-    for val in output_dict_deliveries:
-        print(val)
-
     exist_list = list(filter(lambda x: x in output_dict_routes, output_dict_deliveries))
-    print("exist_list")
-
-    for val in exist_list:
-        print(val)
 
     def compare(s, t):
         return sorted(s) == sorted(t)
@@ -39,14 +29,9 @@ def test_check_carrying_capacity():
     input_dict_route = json.loads(response_route.text)
 
     carrying_capacity = input_dict_route['planned_route']['resource']['carrying_capacity']
-    print("carrying_capacity :", carrying_capacity)
 
-    print("weights")
     weights = [x['algorithm_fields']['weight'] for x in input_dict_route['planned_route']['deliveries'] if
                'weight' in x['algorithm_fields']]
-    for val in weights:
-        print(val)
-    print("sum", sum(weights))
 
     assert carrying_capacity >= sum(weights)
 
@@ -55,16 +40,11 @@ def test_check_all_etas_min_max_route_time():
     response_route = client.get('/route')
     input_dict_route = json.loads(response_route.text)
 
-    print("eta_list")
     eta_list = [x['algorithm_fields']['eta'] for x in input_dict_route['planned_route']['deliveries'] if
                 'eta' in x['algorithm_fields']]
-    for val in eta_list:
-        print(val)
 
     route_min_time = input_dict_route['planned_route']['route_min_time']
-    print("route_min_time: ", route_min_time)
     route_max_time = input_dict_route['planned_route']['route_max_time']
-    print("route_max_time: ", route_max_time)
 
     for val in eta_list:
         assert route_min_time <= val <= route_max_time
@@ -74,23 +54,32 @@ def test_check_all_etas_min_max_delivery_time():
     response_route = client.get('/route')
     input_dict_route = json.loads(response_route.text)
 
-    print("eta_list")
     eta_list = list([x['algorithm_fields']['eta'] for x in input_dict_route['planned_route']['deliveries'] if
                      'eta' in x['algorithm_fields']])
-    for val in eta_list:
-        print(val)
 
-    print("delivery_min_time_list")
     min_time_list = list([x['min_time'] for x in input_dict_route['planned_route']['deliveries']])
-    for val in min_time_list:
-        print(val)
-
-    print("delivery_max_time_list")
     max_time_list = list([x['max_time'] for x in input_dict_route['planned_route']['deliveries']])
-    for val in max_time_list:
-        print(val)
 
     assert len(eta_list) == len(min_time_list) == len(max_time_list)
 
     for i in range(len(eta_list)):
         assert min_time_list[i] <= eta_list[i] <= max_time_list[i]
+
+
+def test_check_travel_time_to_next():
+    response_route = client.get('/route')
+    input_dict_route = json.loads(response_route.text)
+
+    eta_list = list([x['algorithm_fields']['eta'] for x in input_dict_route['planned_route']['deliveries'] if
+                     'eta' in x['algorithm_fields']])
+
+    time_to_next_list = list(
+        [x['algorithm_fields']['time_to_next'] for x in input_dict_route['planned_route']['deliveries'] if
+         'time_to_next' in x['algorithm_fields']])
+
+    assert len(eta_list) == len(time_to_next_list) + 1
+
+    for i in range(len(time_to_next_list)):
+        assert time_to_next_list[i] <= (
+                datetime.datetime.strptime(eta_list[i + 1], "%Y-%m-%dT%H:%M:%S.%fZ") - datetime.datetime.strptime(
+            eta_list[i], "%Y-%m-%dT%H:%M:%S.%fZ")).total_seconds()
